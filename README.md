c872c5422d4d9db023b472c8144be47a603f518f
Krishna Kammaje <Krishn. Kammaje@homemadesupport.com.au>
2024-11-14 11:45:59 +0530

    adding solutions

--- README.md
+++ README.md
@@ -6,3 +6,36 @@
 - run `cds bind auth -2 transactionPoC-auth` to bind the auth service
 - run `cds bind db -2 transactionPoC-db` to bind the db service
 - run `cds watch --profile hybrid` to start the service
+
+
+# Issues and Solutions
+We will discuss various issues with v7 upgrade and how to solve them.
+
+## Issue 1:
+Demoed in `processDataP1` request handler.   
+Within a request handler, if we have DB calls that are not awaited, the transaction will error out with error "❗️Uncaught Error: Transaction is already closed". 
+
+### Analysis
+This is because CAP creates an implicit transaction for the request handler. If we don't await the DB calls, the transaction will close before the DB calls are completed, leading to the error.
+
+### Solution 1
+This is implemented in `processDataP1S1` request handler.   
+`await` the function calls that make the DB calls. 
+
+### Solution 2
+This is implemented in `processDataP1S2` request handler.   
+Within the non-awaited functions making DB calls, use a new transaction.
+
+### Solution 3
+This is implemented in `processDataP1S3` request handler. 
+Wrap the entire content within a transaction. 
+This DOES NOT WORK. It coninues to get the same error "❗️Uncaught Error: Transaction is already closed".
+
+## Issue 2
+Behaviour of transaction in implict mode.
+When you call action `implictTransaction`, it creates an implicit transaction. Within the action it creates MainEntity successfully. But then it creates ChildEntity which causes an error. Since this is an implicit transaction, the error causes the MainEntity transaction to roll back.
+
+
+
+
+
--- srv/service-model.js
+++ srv/service-model.js
@@ -1,6 +1,6 @@
 export default async function() {
     
-    this.on('processDataWrong', async (req) => {
+    this.on('processDataP1', async (req) => {
         
         //Insert entry to the MainEntity
         const parent = await insertMainEntity(req);
@@ -8,13 +8,28 @@ export default async function() {
         //Insert entry to the ChildEntity
         await insertChildEntity(parent, req);
 
-        //Insert another entry to the ChildEntity
+        //Insert another entry to the ChildEntity.
+        //This fails with error "❗️Uncaught Error: Transaction is already closed".
+        //However the above two DB calls are successful and not rolled back.
         insertAnotherChildEntity(parent, req);
 
-        return `Out of processDataWrong`;
+        return `Out of processDataP1`;
     });
 
-    this.on('processDataRight', async (req) => {
+    this.on('processDataP1S1', async (req) => {
+        //Insert entry to the MainEntity
+        const parent = await insertMainEntity(req);
+
+        //Insert entry to the ChildEntity without await
+        await insertChildEntity(req);
+
+        //Insert another entry to the ChildEntity with await. This is successful.
+        await insertAnotherChildEntity(req);
+
+        return `Out of processDataP1S1`;
+    });
+
+    this.on('processDataP1S2', async (req) => {
         //Insert entry to the MainEntity
         const parent = await insertMainEntity(req);
 
@@ -22,9 +37,37 @@ export default async function() {
         await insertChildEntity(req);
 
         //Insert another entry to the ChildEntity
-        await insertAnotherChildEntity(req);
+        insertAnotherChildEntityNewTx(req);   
 
-        return `Out of processDataRight`;
+        return `Out of processDataP1S2`;
+    });
+
+    //This solution does not work. It continues to throw "❗️Uncaught Error: Transaction is already closed".
+    this.on('processDataP1S3', async (req) => {
+        //Wrap the entire content within a transaction.
+        const tx = cds.tx();
+        const output = await tx.run(async () => {
+            //Insert entry to the MainEntity
+            const parent = await insertMainEntity(req);
+
+            //Insert entry to the ChildEntity
+            await insertChildEntity(parent, req);
+
+            //Insert another entry to the ChildEntity without await. 
+            insertAnotherChildEntity(parent, req);
+        });
+
+        return `Out of processDataP1S3`;
+    });
+
+    this.on('implictTransaction', async (req) => {
+        //Insert entry to the MainEntity
+        const parent = await insertMainEntity(req);
+
+        //Insert error causing entry. This will cause the above Main Entry transaction to roll back.
+        await insertChildEntityWithError(req);
+
+        return `Out of implictTransaction`;
     });
 }
 
@@ -42,7 +85,7 @@ async function insertMainEntity(req) {
 //A function to insert entry to the ChildEntity
 async function insertChildEntity(req) {
     const db = await cds.connect.to("db");
-    const { MainEntity, ChildEntity } = db.entities;    
+    const { ChildEntity } = db.entities;    
     const response = await INSERT.into(ChildEntity).entries({
         name: "Child Entity 1",
         description: "First child entity"
@@ -53,6 +96,7 @@ async function insertChildEntity(req) {
 //A function to insert another entry to the ChildEntity
 async function insertAnotherChildEntity(req) {
     const db = await cds.connect.to("db");
+    const newTx = cds.tx(db);
     const { ChildEntity } = db.entities;    
     const response = await INSERT.into(ChildEntity).entries({
         name: "Child Entity 2",
@@ -60,6 +104,37 @@ async function insertAnotherChildEntity(req) {
     });
     return response;
 }
+
+async function insertChildEntityWithError(req) {
+    const db = await cds.connect.to("db");
+    const { ChildEntity } = db.entities;    
+    const response = await INSERT.into(ChildEntity).entries({
+        name: 3, //This will cause an error
+        description: "Third child entity"
+    });
+    return response;
+}
+
+async function insertAnotherChildEntityNewTx(req) {
+    const db = await cds.connect.to("db");
+    const tx = cds.tx(db);
+
+    try {
+        const { ChildEntity } = db.entities;    
+        const response = await tx.run(
+            INSERT.into(ChildEntity).entries({
+                name: "Child Entity 2",
+                description: "Second child entity"
+            })
+        );
+        
+        await tx.commit();
+        return response;
+    } catch (error) {
+        await tx.rollback();
+        throw error;
+    }
+}    
 
 // A function to get all entries from the MainEntity
 async function getAllMainEntities(req) {
